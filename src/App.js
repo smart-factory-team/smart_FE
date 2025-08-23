edbd43026f05c02654256017b43307f86ab653a8
doyooon <ian661087@gmail.com>
2025-08-23 19:18:45 +0900

    refactor: update GlobalStyle css in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 import { AppRouter } from './router';
 
 // 전역 스타일
-const GlobalStyle = styled.div`
+const GlobalStyle = createGlobalStyle`
   * {
     margin: 0;
     padding: 0;
@@ -18,9 +18,10 @@ const GlobalStyle = styled.div`
 
 function App() {
   return (
-    <GlobalStyle>
+    <>
+      <GlobalStyle />
       <AppRouter />
-    </GlobalStyle>
+    </>
   );
 }
 
